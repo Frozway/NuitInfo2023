02762dc47c226b7df1ab1e0186317f799b391498
rafrezzz <rafael.freitas.78360@gmail.com>
2023-12-08 06:03:12 +0100

    Loader qui marche a moitier

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,17 @@
 const loader = document.querySelector('.loader');
+const loaderTitle = document.getElementById('loaderTitle'); // Modification de l'ID pour correspondre à celui dans le HTML
 
 window.addEventListener('load', () => {
     setTimeout(() => {
         loader.classList.add('hidden');
+        loaderTitle.classList.remove('hidden');
+        setTimeout(() => {}, 1000);
     }, 1500);
+});
+
+document.addEventListener('keydown', (event) => {
+    const key = event.key.toLowerCase();
+    if (key === 'z' || key === 'q' || key === 'd') {
+        loader.classList.add('hidden'); // Ajout de la classe hidden seulement au loader, pas au titre
+    }
 });
